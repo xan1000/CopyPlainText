a0fdfe41fa83a1c699de2dace21f498fdaab14d2
Matthew Bolger <matthew.bolger@rmit.edu.au>
2018-08-25 10:47:19 +1000

    Updated packages, supported engine and dependencies.

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
-'use strict';
-import * as vscode from 'vscode';
+"use strict";
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
     // node-copy-paste https://github.com/xavi-/node-copy-paste
     const ncp = require("copy-paste");
 
-    const disposable = vscode.commands.registerCommand('extension.copyPlainText', () => {
+    const disposable = vscode.commands.registerCommand("extension.copyPlainText", () => {
         const editor = vscode.window.activeTextEditor;
         if(!editor) {
             return; // No open text editor.
